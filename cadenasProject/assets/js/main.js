90f492f74b6d5f58e3e696dd5854e3f770fc5bdb
Hellen Ruiz <estefany25rb@gmail.com>
2025-11-28 15:10:24 -0500

    Primera versión

---
+++ cadenasProject/assets/js/main.js
@@ -0,0 +1,18 @@
+/*Cadenas en JavaScript
+*
+*/
+
+let fruitName1 = "Manzana roja"; //Una cadena de texto con comillas dobles
+let fruitName2 = `Manzana verde`; //Una cadena de texto de comisas invertidas
+
+window.alert("Fruta 1: " + fruitName1);
+window.alert("Fruta 2: " + fruitName2);
+
+let text = "We are the so-called \'Vikings\' from the north.";
+window.alert(text);
+
+let texto = "S\tE\tN\tA";
+window.alert(texto);
+
+let longitud = text.length;
+window.alert(longitud);
